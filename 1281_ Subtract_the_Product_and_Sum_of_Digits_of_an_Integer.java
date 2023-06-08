5a0b3f4bbff112fba8076bf484942105721ec06e
CHAITANYA KATORE <106269976+chaitanyakatore@users.noreply.github.com>
2023-06-08 23:23:04 +0530

    Create 1281_ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java

---
+++ 1281_ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java
@@ -0,0 +1,18 @@
+//1281. Subtract the Product and Sum of Digits of an Integer
+
+
+//using the modulo and reminder method
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int sum =0;
+        int product = 1;
+        while(n>0){
+            int num = n%10;
+            n/=10;
+
+            sum +=num;
+            product *= num;
+        }
+        return product - sum;
+    }
+}
